87fcec4e21fc2bd65cd46251abd7443eb3e8af2a
Rafael Figueira <61553927+RafaelSFigueira@users.noreply.github.com>
2022-06-09 19:14:23 +0100

    Update action.php

--- action.php
+++ action.php
@@ -1,11 +1,8 @@
-<?php  
-    if(isset($_POST['submit']))  
-    {  
-        $number1 = $_POST['number1'];  
-        $number2 = $_POST['number2'];  
-        $sum =  $number1+$number2;
-	$sum2 =  $number1-$number2;      
-        echo "The difference of $number1 and $number2 is: ".$sum; 
-	echo "The sum of $number1 and $number2 is: ".$sum2;   
-    }  
-?>  
+<?php
+   if( $_GET["number1"] || $_GET["number2"] ) {
+      $sum =  $_GET["number1"]+$_GET["number2"];
+      echo "The sum of $number1 and $number2 is: ".$sum; 
+      
+      exit();
+   }
+?>
